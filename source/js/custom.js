db0b86778f97d33c018b23ba19ea078ddfa0e6a7
xiong <wooiseongseong96@gmail.com>
2024-12-31 08:25:24 +0800

    add custom.css

---
+++ source/js/custom.js
@@ -0,0 +1,13 @@
+// let getCodeType = document.getElementsByTagName('code');
+// let attrData = document.querySelector('.code-wrapper');
+// let codeType = [];
+
+// for (let i = 0; i < getCodeType.length; i++) {
+//  codeType = getCodeType[i].className.split(" ")[1];
+//  attrData[i].setAttribute("data-rel", codeType);
+//   console.log(codeType)
+
+//  codeType === 'applescript' ?
+//   attrData[i].setAttribute("data-rel", '') :
+//   attrData[i].setAttribute("data-rel", codeType);
+// };
